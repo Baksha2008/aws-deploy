f7d00923ce1116f8001b249bc27e92713aeb19fe
Baksha2008 <baksha2008@gmail.com>
2019-06-10 13:33:17 +0300

    last test

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello Hello Hello Hello hello World!';
+    return 'Last test!!!';
   }
 }
