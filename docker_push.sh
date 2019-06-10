0893ab6ef8614d0ac1bdf99845f976769c29a995
Baksha2008 <baksha2008@gmail.com>
2019-06-10 13:24:34 +0300

    i hope its wokrs2

--- docker_push.sh
+++ docker_push.sh
@@ -1,12 +1,7 @@
 #!/bin/bash -e
 
-# the registry should have been created already
-# you could just paste a given url from AWS but I'm
-# parameterising it to make it more obvious how its constructed
 REGISTRY_URL=${AWS_ACCOUNT_ID}.dkr.ecr.us-east-2.amazonaws.com
-# this is most likely namespaced repo name like myorg/veryimportantimage
 SOURCE_IMAGE="test:latest"
-# using it as there will be 2 versions published
 TARGET_IMAGE="${REGISTRY_URL}/test:latest"
 
 # Push image to ECR
@@ -17,15 +12,9 @@ TARGET_IMAGE="${REGISTRY_URL}/test:latest"
 # in environmental vars
 $(aws ecr get-login --no-include-email --region us-east-2)
 
-
-
-
 # update latest version
 docker tag ${SOURCE_IMAGE} ${TARGET_IMAGE}
 docker push ${TARGET_IMAGE}
 
-aws ecs update-service \
-  --cluster my-cluster-name \
-  --service web \
-  --task-definition test \
-  --force-new-deployment
+# update service and task
+aws ecs update-service --cluster test --service blablabla --task-definition test --force-new-deployment
