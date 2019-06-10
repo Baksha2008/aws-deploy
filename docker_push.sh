aef06fd4e7fe0e20bf2519e163306cf2ea9044ba
Baksha2008 <baksha2008@gmail.com>
2019-06-10 12:58:59 +0300

    update15 text

--- docker_push.sh
+++ docker_push.sh
@@ -3,18 +3,11 @@
 # the registry should have been created already
 # you could just paste a given url from AWS but I'm
 # parameterising it to make it more obvious how its constructed
-REGISTRY_URL=${AWS_ACCOUNT_ID}.dkr.ecr.${EB_REGION}.amazonaws.com
+REGISTRY_URL=${AWS_ACCOUNT_ID}.dkr.ecr.us-east-2.amazonaws.com
 # this is most likely namespaced repo name like myorg/veryimportantimage
 SOURCE_IMAGE="test:latest"
 # using it as there will be 2 versions published
 TARGET_IMAGE="${REGISTRY_URL}/test:latest"
-
-# making sure correct region is set
-aws configure set default.region ${EB_REGION}
-
-aws configure set aws_access_key_id ${AWS_ACCESS_KEY_ID}
-aws configure set aws_secret_access_key ${AWS_SECRET_ACCESS_KEY}
-aws configure set default.output_format 'json'
 
 # Push image to ECR
 ###################
