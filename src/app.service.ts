2b009819c26b6c037fd5753c7594e413ad61e3a7
Baksha2008 <baksha2008@gmail.com>
2019-06-13 18:47:03 +0300

    asas

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Again wwtest!!!';
+    return 'Again wwtsssest!!!';
   }
 }
